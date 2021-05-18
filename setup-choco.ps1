32a8844a0f3a2cc8cdbcf4b9a325686698783574
Matt Wicks <matt.wicks@gmail.com>
2021-05-18 11:18:31 +1000

    ✨ separated dev tools into optional category

--- setup-choco.ps1
+++ setup-choco.ps1
@@ -1,6 +1,6 @@
 #install choco
 Write-Host "Installing/Updating Choco"
-if ((Get-Command -Name choco.exe -ErrorAction SilentlyContinue) -eq $null) {
+if ($null -eq (Get-Command -Name choco.exe -ErrorAction SilentlyContinue)) {
     Set-ExecutionPolicy Bypass -Scope Process -Force
     [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
     Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
@@ -8,41 +8,42 @@ if ((Get-Command -Name choco.exe -ErrorAction SilentlyContinue) -eq $null) {
     & choco update Chocolatey
 }
 
-# add tools
-Write-Host  "Intstalling tools"
+Write-Host  "Intstalling minimal setup"
 & choco install `
     7zip `
-    autohotkey.install `
-    azure-cli `
     cascadia-code-nerd-font `
     chocolateygui `
-    docker-desktop `
-    eartrumpet `
     git `
-    gh `
-    insomnia-rest-api-client `
     oh-my-posh `
-    linqpad `
-    logitech-camera-settings `
-    logitech-options `
     microsoft-windows-terminal `
-    microsoft-teams `
-    nodejs-lts `
     notepadplusplus `
-    nswagstudio `
-    obs-studio `
-    obs-virtualcam `
     paint.net `
-    postman `
     powertoys `
     pwsh `
-    sudo `
     sysinternals `
-    vscode `
     which `
     -y
 
-if (Read-Host "Install paid tools: (Y/N)" -eq 'y') {
+
+# add tools
+if ((Read-Host "Install dev tools: (y/N)").ToLower() -eq 'y') {
+    & choco install `
+        autohotkey.install `
+        azure-cli `
+        docker-desktop `
+        eartrumpet `
+        gh `
+        insomnia-rest-api-client `
+        linqpad `
+        nodejs-lts `
+        nswagstudio `
+        postman `
+        sudo `
+        vscode `
+        -y
+}
+
+if ((Read-Host "Install paid tools: (y/N)").ToLower() -eq 'y') {
     & choco install `
         1password `
         camtasia `
@@ -52,9 +53,19 @@ if (Read-Host "Install paid tools: (Y/N)" -eq 'y') {
         -y
 }
 
-if (Read-Host "Install gaming apps: (Y/N)" -eq 'y') {
+if ((Read-Host "Install gaming apps: (y/N)").ToLower() -eq 'y') {
     & choco install `
         epicgameslauncher `
         steam-client `
+        -y
+}
+
+if ((Read-Host "Install other apps: (y/N)").ToLower() -eq 'y') {
+    & choco install `
+        logitech-camera-settings `
+        logitech-options `
+        microsoft-teams `
+        obs-studio `
+        obs-virtualcam `
         -y
 }
